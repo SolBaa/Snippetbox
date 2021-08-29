f42f5e56245865856bbe7bcb155f9db7c38ff0ee
Sol Battaglia <solbattaglia@gmail.com>
2021-08-29 16:39:40 -0300

    [TECH]-Added assets

--- main.go
+++ main.go
@@ -5,29 +5,16 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello Sol "))
-}
-
-func showSnippetbox(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from snippetbox"))
-}
-
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from snippetbox"))
-}
-
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippetbox)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", Home)
+	mux.HandleFunc("/snippet", ShowSnippet)
+	mux.HandleFunc("/snippet/create", CreateSnippet)
 
-	log.Println("Starting server on port :4000")
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
